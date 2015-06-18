7e2fb7114336fe1d00237f05b99e1b29e1585dc9
zardak <zardakss@gmail.com>
2015-06-19 02:06:43 +0300

    Bumped package version to 0.9.1

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'zardak:soap',
-  version: '0.0.1',
+  version: '0.1.0',
   summary: 'Wrapped npm soap package',
   git: 'https://github.com/zardak/meteor-soap',
   documentation: 'README.md'
 });
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
   api.versionsFrom('0.9.3');
   api.use('coffeescript');
   api.addFiles('soap.coffee', 'server');
 });
 
 Npm.depends({
-  soap: '0.7.0'
+  soap: '0.9.1'
 });
