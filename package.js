8ce9101850017cd3c278e2b50eebf5b0274e5821
zardak <zardak@users.noreply.github.com>
2015-09-28 01:34:44 +0300

    Bumped package version to 0.9.3

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'zardak:soap',
-  version: '0.2.3',
+  version: '0.2.4',
   summary: 'Wrapped npm soap package',
   git: 'https://github.com/zardak/meteor-soap',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  soap: '0.9.1'
+  soap: '0.9.3'
 });
 
 Package.onUse(function(api) {
